4a74f539430ce63d1e8aa087340a08a3d29f2ffc
JAINIL SHAH <71964479+jainilshah22@users.noreply.github.com>
2023-07-12 18:23:52 +0530

    Add files via upload

---
+++ weatherapp.py
@@ -0,0 +1,16 @@
+import requests
+api_key = '3c91f2ad78ccc17451c13dd68F107148'
+
+city=input("Enter City Name:")
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    temp = data['main']['temp']
+    desc = data['weather'][0]['description']
+    print(f'Temperature: {temp} K')
+    print(f'Description: {desc}')
+else:
+    print('Error fetching weather data')
